caaa9ea3fb7057ce29fb48bc635c04c33477d4f0
Ashley Akins <ashleyakins2015@hotmail.com>
2023-08-13 22:45:22 -0500

    update

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-
+from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from .models import Finch
 
 def home(request):
   return render(request, 'home.html')
@@ -8,8 +10,28 @@ def about(request):
   return render(request, 'about.html')
 
 def finches_index(request):
-    return render(request, 'finches/index.html', {
-      'finches': finches
-    })
+    finches = Finch.objects.all()
+    return render(request, 'finches/index.html', 
+    { 
+        'finches': finches 
+    }
+)
 
+def finches_detail(request, finch_id):
+  finch = Finch.objects.get(id=finch_id)
+  return render(request, 'finches/detail.html', { 'finch': finch })
 
+class FinchCreate(CreateView):
+    model = Finch
+    fields = '__all__'
+
+    success_url = '/finches/{finch_id}'
+
+class FinchUpdate(UpdateView):
+  model = Finch
+
+  fields = ['breed', 'description', 'age']
+
+class FinchDelete(DeleteView):
+  model = Finch
+  success_url = '/finches'
